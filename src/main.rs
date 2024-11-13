40c4b25ad41c23de2ac24eb3257793db7cdda6ad
Logan Cox <mail@logan-cox.com>
2024-11-13 16:10:17 +0000

    clippy warnings

--- src/main.rs
+++ src/main.rs
@@ -11,7 +11,7 @@ async fn main() -> Result<(), std::io::Error> {
     init_subscriber(subscriber);
 
     let configuration = get_configuration().expect("Failed to read configuration.");
-    let connection = PgPool::connect(&configuration.database.connection_string().expose_secret())
+    let connection = PgPool::connect(configuration.database.connection_string().expose_secret())
         .await
         .expect("Failed to connect to Postgres.");
 
