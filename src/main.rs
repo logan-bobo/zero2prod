21b244e95464b0ddfefda13fcbed5f959005b1a7
Logan Cox <mail@logan-cox.com>
2024-11-14 09:35:25 +0000

    add app spec and allow for database connections over SSL

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,4 @@
-use secrecy::ExposeSecret;
-use sqlx::PgPool;
+use sqlx::postgres::PgPoolOptions;
 use std::net::TcpListener;
 use zero2prod::configuration::get_configuration;
 use zero2prod::startup::run;
@@ -11,9 +10,8 @@ async fn main() -> Result<(), std::io::Error> {
     init_subscriber(subscriber);
 
     let configuration = get_configuration().expect("Failed to read configuration.");
-    let connection = PgPool::connect(configuration.database.connection_string().expose_secret())
-        .await
-        .expect("Failed to connect to Postgres.");
+    let connection =
+        PgPoolOptions::new().connect_lazy_with(configuration.database.connect_options());
 
     let address = format!(
         "{}:{}",
